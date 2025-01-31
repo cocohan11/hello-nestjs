fb2318b3c4fa456339b4df7b93a38389bc24b9a6
han <rlagksdl96@naver.com>
2025-01-31 18:16:10 +0900

    8.2.15 One to One Relationship Read 작업 해보기

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -43,7 +43,8 @@ export class MovieService {
     const movie = await this.movieRepository.findOne({
       where:{
         id,
-      }
+      },
+      relations: ['detail']
     })
      
     if(!movie) {
